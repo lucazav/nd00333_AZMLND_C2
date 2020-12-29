6c11f2e87a4c9b5b43c14553bf6720c99d4461a4
Luca Zavarella <lucazavarella@outlook.com>
2020-12-29 12:04:46 +0000

    Fixed some bugs and added swagger file

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,7 +5,7 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "bank-marketing-scoring"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
@@ -13,3 +13,9 @@ logs = service.get_logs()
 
 for line in logs.split('\n'):
     print(line)
+
+service.update(enable_app_insights=True)
+
+service.enable_app_insights
+
+
